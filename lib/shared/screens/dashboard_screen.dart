5b20c8237e107155db92fdbb3c28f9e2250d6262
Vinabike7 <vinabikechile@gmail.com>
2026-06-25 19:41:49 -0400

    test: brighten dashboard update banner

--- lib/shared/screens/dashboard_screen.dart
+++ lib/shared/screens/dashboard_screen.dart
@@ -31,8 +31,8 @@ class _DashboardScreenState extends State<DashboardScreen> {
                 decoration: BoxDecoration(
                   gradient: const LinearGradient(
                     colors: [
-                      Color(0xFFE65100),
-                      Color(0xFFF57C00),
+                      Color(0xFFEF6C00),
+                      Color(0xFFFF9800),
                     ],
                     begin: Alignment.topLeft,
                     end: Alignment.bottomRight,
